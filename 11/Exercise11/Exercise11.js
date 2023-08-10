ff83763fdfdbbeabc96e165d048b761dbb3b8505
enginoir <myusuf141012@gmail.com>
2023-08-10 19:10:59 +0700

    Initial commit

--- 11/Exercise11/Exercise11.js
+++ 11/Exercise11/Exercise11.js
@@ -2,15 +2,15 @@ const Queue = require('./dataClass');
 
 const queueManager = new Queue();
 
-console.log('● Add orders to queue');
+console.log('●) Add orders to queue');
 queueManager.addOrder(1);
 queueManager.addOrder(2);
 queueManager.addOrder(3);
 queueManager.addOrder(4);
 queueManager.addOrder(5);
 
-console.log('● [Log] Print all data in queue');
+console.log('[Log] Print all data in queue');
 queueManager.printQueue();
 
-console.log('● Run process -> process queue based on random interval');
+console.log('●) Run process -> process queue based on random interval');
 queueManager.processQueue();
